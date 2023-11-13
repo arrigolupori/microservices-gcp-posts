89493f514bfc0370d40947e85f99d93c12a821d4
Arrigo Lupori <91458307+arrigolupori@users.noreply.github.com>
2023-11-13 22:34:18 +0000

    updated client to minimize requests via query service

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -7,7 +7,7 @@ const PostList = () => {
     const [posts, setPosts] = useState({});
 
     const fetchPosts = async () => {
-        const res = await axios.get("https://redesigned-space-eureka-gww46gjpwqv2vjv9-4000.app.github.dev/posts");
+        const res = await axios.get("https://redesigned-space-eureka-gww46gjpwqv2vjv9-4002.app.github.dev/posts");
 
         setPosts(res.data);
     };
@@ -25,7 +25,7 @@ const PostList = () => {
             >
                 <div className="card-body">
                     <h3>{post.title}</h3>
-                    <CommentList postId={post.id} />
+                    <CommentList comments={post.comments} />
                     <CommentCreate postId={post.id} />
                 </div>
             </div>
